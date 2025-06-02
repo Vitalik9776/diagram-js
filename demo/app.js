d7a05eab6db9ec9595238726366623e80a44f10d
Vitalik9776 <85066856+Vitalik9776@users.noreply.github.com>
2025-06-02 12:19:23 +0200

    feat: auto layout and modules for hierarchy demo

--- demo/app.js
+++ demo/app.js
@@ -2,15 +2,64 @@ import Diagram from '../lib/Diagram';
 import HierarchyModule from '../lib/features/hierarchy';
 import MoveCanvasModule from '../lib/navigation/movecanvas';
 import ZoomScrollModule from '../lib/navigation/zoomscroll';
+
+import ConnectModule from '../lib/features/connect';
+import ContextPadModule from '../lib/features/context-pad';
+import CreateModule from '../lib/features/create';
+import HandToolModule from '../lib/features/hand-tool';
+import LassoToolModule from '../lib/features/lasso-tool';
+import SpaceToolModule from '../lib/features/space-tool';
+import GlobalConnectModule from '../lib/features/global-connect';
+import ModelingModule from '../lib/features/modeling';
+import MoveModule from '../lib/features/move';
+import OutlineModule from '../lib/features/outline';
+import PaletteModule from '../lib/features/palette';
+import ResizeModule from '../lib/features/resize';
+import RulesModule from '../lib/features/rules';
+import SelectionModule from '../lib/features/selection';
+import LabelSupportModule from '../lib/features/label-support';
+import PopupMenuModule from '../lib/features/popup-menu';
+import OverlaysModule from '../lib/features/overlays';
+import TranslateModule from '../lib/i18n/translate';
+import SnappingModule from '../lib/features/snapping';
+import EditorActionsModule from '../lib/features/editor-actions';
+import KeyboardModule from '../lib/features/keyboard';
 import data from '../lib/features/hierarchy/sampleData';
+import autoLayout from '../lib/features/hierarchy/autoLayout';
 
 const canvas = document.querySelector('#canvas');
 
 const diagram = new Diagram({
   canvas: { container: canvas },
-  modules: [ MoveCanvasModule, ZoomScrollModule, HierarchyModule ]
+  modules: [
+    MoveCanvasModule,
+    ZoomScrollModule,
+    ConnectModule,
+    ContextPadModule,
+    CreateModule,
+    HandToolModule,
+    LassoToolModule,
+    SpaceToolModule,
+    GlobalConnectModule,
+    ModelingModule,
+    MoveModule,
+    OutlineModule,
+    PaletteModule,
+    ResizeModule,
+    RulesModule,
+    SelectionModule,
+    LabelSupportModule,
+    PopupMenuModule,
+    OverlaysModule,
+    TranslateModule,
+    SnappingModule,
+    EditorActionsModule,
+    KeyboardModule,
+    HierarchyModule
+  ]
 });
 
+autoLayout(data);
 diagram.get('hierarchyModeling').load(data);
 
 // show element details on click
